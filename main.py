ca79fa3359a184ad1506312a8867e20b08ff5c51
Nykko-Volkov <nathannunesmoreira1@gmail.com>
2025-08-01 15:33:11 -0300

    fianlly done some other corrections

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 import flask 
+from flask import Flask, render_template,request,redirect
 from db import Databases
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 decorator = app.route
 todo_db = Databases('mytodo.db')
    
 @decorator('/')
 def ind():
     todos = todo_db.get_all()
-    return flask.render_template('index.html',nathan_todo=todos)
+    return render_template('index.html',nathan_todo=todos)
 
 
 
 @decorator('/save',methods=['post'])
 def ind2():
-    data = flask.request.form['inputfromhtml']
+    data = request.form['inputfromhtml']
     todo_db.add(data)
-    return flask.redirect('/')
+    return redirect('/')
 
 @decorator('/delete/<path:todo>',methods=['post'])
 def delete(todo):
     todo_db.delete(todo)
-    return flask.redirect('/')
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
